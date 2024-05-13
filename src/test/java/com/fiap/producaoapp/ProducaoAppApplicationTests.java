ea29de9682d6384e6309e4c0a697866fed0088b4
Thiago <thiagobsn@gmail.com>
2024-05-12 23:54:36 -0300

    Teste

--- src/test/java/com/fiap/producaoapp/ProducaoAppApplicationTests.java
+++ src/test/java/com/fiap/producaoapp/ProducaoAppApplicationTests.java
@@ -1,17 +1,7 @@
 package com.fiap.producaoapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-
-
-@ActiveProfiles("test")
-@SpringBootTest
 class ProducaoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
