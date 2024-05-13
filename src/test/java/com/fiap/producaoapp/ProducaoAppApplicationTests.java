8ab0bd705e522b21657986dc8bf7134fa09cc191
Thiago <thiagobsn@gmail.com>
2024-05-13 00:15:03 -0300

    Ajuste test

--- src/test/java/com/fiap/producaoapp/ProducaoAppApplicationTests.java
+++ src/test/java/com/fiap/producaoapp/ProducaoAppApplicationTests.java
@@ -1,5 +1,11 @@
 package com.fiap.producaoapp;
 
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase
+@SpringBootTest
 class ProducaoAppApplicationTests {
 
 	
